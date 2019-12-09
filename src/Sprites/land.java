2a0ef01387700645292fee1fbfe45015e8874156
infernalxdrm <36190565+infernalxdrm@users.noreply.github.com>
2019-12-09 11:19:49 +0700

    держи скобки

--- src/Sprites/land.java
+++ src/Sprites/land.java
@@ -9,8 +9,7 @@ import java.awt.*;
 public class land extends PhysicObject {
     int x1,x2,y1,y2;
 
-    public land(int x1, int x2, int y1, int y2) 
-    {
+    public land(int x1, int x2, int y1, int y2) {
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
@@ -21,8 +20,7 @@ public class land extends PhysicObject {
     public void update(GameCanvas canvas, float deltaTime) {}
 
     @Override
-    public void render(GameCanvas canvas, Graphics g) 
-    {
+    public void render(GameCanvas canvas, Graphics g) {
         g.drawLine(x1,y1,x2,y2);
     }
 }
